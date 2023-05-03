5ef18908327198ca599321e408bfc22b35bc51a2
HiImDiegoVinicius <diegovinicius.dev@gmail.com>
2023-05-03 01:00:32 -0300

    first commit

--- src/App.java
+++ src/App.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
 public class App {
-    public static void main(String[] args) throws Exception {
-       System.out.println("Alô, Mundo");
-    }
+    public static void main(String[] args) {
+    System.out.print("Digite um núemro: ");
+    Scanner input = new Scanner(System.in);
+    
+    int numero = input.nextInt();
+    System.out.println("Número Digitado: " + numero);
+    
+       
+}
 }
